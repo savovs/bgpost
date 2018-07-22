5846c2e23cbe588842c146fa9ff065be4875a2d2
Sasho Savov <savovs@gmail.com>
2018-07-22 13:32:41 +0300

    Added file delivery_en.html

--- src/opencart_prices.py
+++ src/opencart_prices.py
@@ -6,7 +6,6 @@ from pprint import pprint
 TAX_BGN = 2.93
 
 pd.set_option("precision", 2)
-df = pd.read_csv("../land_prices.csv")
 
 def format_weight(row):
     # Remove symbols and whitespace
@@ -20,16 +19,6 @@ def format_weight(row):
 
     # Convert strings to numbers
     return pd.to_numeric(row, errors="coerce")
-
-df = df.apply(format_weight, axis=1)
-
-# Convert weights to grams 
-weights = df["weight_kg"].values
-weights = weights * 1000
-
-df = df.drop("weight_kg", axis=1)
-
-prices_all_zones = dict()
 
 def add_tax(data_tuple):
     index, price = data_tuple
@@ -50,22 +39,38 @@ def clean_dict_string(dictionary):
 
     return string.translate({ ord(char): "" for char in chars_to_remove })
 
+for index, file_path in enumerate(["../land_prices.csv", "../air_prices.csv"]):
+    df = pd.read_csv(file_path)
+    df = df.apply(format_weight, axis=1)
+
+    # Convert weights to grams 
+    weights = df["weight_kg"].values
+    weights = weights * 1000
+
+    df = df.drop("weight_kg", axis=1)
+
+    prices_all_zones = dict()
+
+    path = os.path.dirname(os.path.realpath(__file__))
+
+    if index == 0:
+        name = 'land_price'
+    else:
+        name = 'air_price'
+
+    file_path = path + "/../opencart_shipping_{}_strings_parcels.txt".format(name)
+
+    text_file = open(file_path, "w")
+
+    for index, zone_name in enumerate(df):
+        current_prices = df[zone_name]
+        current_prices = map(add_tax, enumerate(current_prices))
+        current_prices = map(format_decimal_places, current_prices)
+        
+        prices_all_zones[zone_name] = dict(zip(weights, current_prices))
+
+        text_file.write(zone_name + "\n")
+        text_file.write(clean_dict_string(prices_all_zones[zone_name]) + "\n\n")
 
 
-path = os.path.dirname(os.path.realpath(__file__))
-file_path = path + "/../opencart_shipping_price_strings.txt"
-
-text_file = open(file_path, "w")
-
-for index, zone_name in enumerate(df):
-    current_prices = df[zone_name]
-    current_prices = map(add_tax, enumerate(current_prices))
-    current_prices = map(format_decimal_places, current_prices)
-    
-    prices_all_zones[zone_name] = dict(zip(weights, current_prices))
-
-    text_file.write(zone_name + "\n")
-    text_file.write(clean_dict_string(prices_all_zones[zone_name]) + "\n\n")
-
-
-text_file.close()
+    text_file.close()
